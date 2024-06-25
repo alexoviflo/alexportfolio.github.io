0adbf309c0e6e4f84d84f574fca9b405dc9db4b3
alexoviflo <alexander.ovesen@online.no>
2024-06-26 00:41:00 +0200

    vite endring

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/navnet-pa-repository/', // Sett basen din her for GitHub Pages
+  build: {
+    outDir: 'dist' // Dette er standard og trenger ikke å settes eksplisitt hvis du bruker 'dist'
+  }
 })
+
