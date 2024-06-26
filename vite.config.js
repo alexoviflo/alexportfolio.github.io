754409bf912722317bd3ac9a9ab599847d819987
Alexander <alexander.ovesen@online.no>
2024-06-26 16:43:29 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/alexxoviflo.github.io/', // Endre dette til navnet på GitHub-repositoryet ditt
+  base: '/', // Endre dette til navnet på GitHub-repositoryet ditt
 })
