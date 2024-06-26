c612c554f858e47d756b8e6ee33d10a6b855f423
alexoviflo <alexander.ovesen@online.no>
2024-06-26 17:51:33 +0200

    add: deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/alexoviflo.github.io/', // Endre dette til navnet på GitHub-repositoryet ditt
+  base : "/alexis.github.io/"
 })
